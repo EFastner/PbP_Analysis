25084a644220511e5aae486faad8574b4aae6052
EFastner <eric.fastner@gmail.com>
2018-10-20 23:24:38 -0500

    modified file to create a master copy of all PBP summaries

--- Game_Results/run_game_results.r
+++ Game_Results/run_game_results.r
@@ -1,9 +1,19 @@
-raw_season <- read_csv("~/Data_Sets/Hockey/Enhanced PbP/pbp_20122013.csv", col_names = TRUE)
+require(readr)
 
-season_summary <- fun.game_result_summary(raw_season) 
+season_IDs <- c("20072008", "20082009", "20092010", "20102011", "20112012", "20122013", "20132014", "20142015", "20152016", "20162017", "20172018")
 
-team_results <- fun.results_by_team(season_summary)
+for (i in 1:length(season_IDs)){
+  
+  raw_season <- read_csv(paste0("~/Data_Sets/Hockey/Enhanced PbP/pbp_", season_IDs[i], ".csv"), col_names = TRUE)
+  
+  season_summary <- fun.game_result_summary(raw_season) 
+  
+  team_results <- fun.results_by_team(season_summary)
+  
+  write_csv(team_results, path = paste0("~/Data_Sets/Hockey/Enhanced PbP/Summarized_Seasons/summary_", season_IDs[i], ".csv"))
+  
+}
 
-team_points <- team_results %>% group_by(season, team) %>% summarise(points = max(team_point_total)) %>% arrange(points)
+#team_points <- team_results %>% group_by(season, team) %>% summarise(points = max(team_point_total)) %>% arrange(points)
 
-team_align_check <- unique(team_results$team) %in% unique(row.names(df.alignment_20112013))
+#team_align_check <- unique(team_results$team) %in% unique(row.names(df.alignment_20112013))
