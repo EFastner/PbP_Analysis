2a5c827043ff47fc8aa1bd6846f88fa0af3882fb
EFastner <eric.fastner@gmail.com>
2018-11-13 23:40:50 -0600

    Added labels to viz and removed legend

--- Game_Results/Lock_Line_Analysis/lock_line_viz.R
+++ Game_Results/Lock_Line_Analysis/lock_line_viz.R
@@ -8,14 +8,25 @@ df.lock_line <- read_csv("~/Data_Sets/Hockey/Enhanced PbP/Summarized_Seasons/loc
 
 df.reg_season_summaries <- filter(df.team_results, session == "R")
 
-team_name <- c("MIN", "NSH")
+team_name <- c("L.A", "CHI")
 season_ID <- "20182019"
 
 df.team_summaries <- filter(df.reg_season_summaries, team %in% team_name & season %in% season_ID) %>% arrange(team_game)
+
 df.lock_line <- filter(df.lock_line, team_game <= max(df.team_summaries$team_game))
+
+df.line_labels <- 
+  df.team_summaries %>% 
+  group_by(team) %>%
+  summarise(points = max(team_point_total),
+            games = max(team_game))
 
 viz.season_compare <- 
   ggplot() +
-  geom_line(data = df.lock_line, na.rm = TRUE, mapping = aes(x = team_game, y = lock_line), color = "green", linetype = "longdash") +
+  ggtitle("Lock Line and Point of No Return") +
+  theme(plot.title = element_text(hjust = 0.5), legend.position = "none") +
+  labs(x = "Game Number", y = "Total Points", color = "Team") +
+  geom_line(data = df.lock_line, na.rm = TRUE, mapping = aes(x = team_game, y = lock_line), color = "blue", linetype = "longdash") +
   geom_line(data = df.lock_line, na.rm = TRUE, mapping = aes(x = team_game, y = no_return), color = "red", linetype = "longdash") +
-  geom_line(data = df.team_summaries, mapping = aes(x = team_game, y = team_point_total, color = team))
+  geom_line(data = df.team_summaries, mapping = aes(x = team_game, y = team_point_total, color = team)) +
+  geom_text(data = df.line_labels, mapping = aes(x = games, y = points, label = team, color = team), nudge_x = .75)
