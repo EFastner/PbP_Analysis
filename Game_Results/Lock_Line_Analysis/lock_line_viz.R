e903d0c515ef92f5c0be61e8417d29e4f8c29a96
EFastner <eric.fastner@gmail.com>
2018-11-13 23:11:19 -0600

    Added ability to graph multiple teams on one viz

--- Game_Results/Lock_Line_Analysis/lock_line_viz.R
+++ Game_Results/Lock_Line_Analysis/lock_line_viz.R
@@ -4,14 +4,15 @@ require(dplyr)
 require(readr)
 
 df.lock_line <- read_csv("~/Data_Sets/Hockey/Enhanced PbP/Summarized_Seasons/lock_line.csv", col_names = TRUE)
-df.game_summaries <- read_csv("~/Data_Sets/Hockey/Enhanced PbP/Summarized_Seasons/all_seasons.csv", col_names = TRUE)
+#df.game_summaries <- read_csv("~/Data_Sets/Hockey/Enhanced PbP/pbp_20182019.csv", col_names = TRUE)
 
-df.reg_season_summaries <- filter(df.game_summaries, session == "R")
+df.reg_season_summaries <- filter(df.team_results, session == "R")
 
-team_name <- c("COL", "MIN")
-season_ID <- "20162017"
+team_name <- c("MIN", "NSH")
+season_ID <- "20182019"
 
-df.team_summaries <- filter(df.reg_season_summaries, team == team_name & season == season_ID) %>% arrange(team_game)
+df.team_summaries <- filter(df.reg_season_summaries, team %in% team_name & season %in% season_ID) %>% arrange(team_game)
+df.lock_line <- filter(df.lock_line, team_game <= max(df.team_summaries$team_game))
 
 viz.season_compare <- 
   ggplot() +
